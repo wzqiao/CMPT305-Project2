102c92fa23baba6952c6d70baf058542ce2193e9
Chase <80236492+Are-u-writing-bugs@users.noreply.github.com>
2022-04-14 18:31:06 -0700

    Add files via upload

---
+++ Queue.cpp
@@ -0,0 +1,40 @@
+#include"Queue.h"
+
+Node::Node(string instruction) {
+	this->instruction = instruction;
+	this->next = NULL;
+}
+
+Queue::Queue() {
+	this->head = NULL;
+	this->tail = NULL;
+}
+
+void push(Queue* q, Node* node) {
+	if (q->head == NULL) {
+		q->head = node;
+		q->tail = node;
+	}
+	else {
+		q->tail = node;
+		q->tail->next = NULL;
+	}
+}
+
+Node* pop(Queue* q) {
+
+	if (q->head == NULL) {
+		return;
+	}
+	/*if (q->head->next == NULL) {
+		Node* temp = q->head;
+		q->head = NULL;
+		return temp;
+	}*/
+
+	else {
+		Node* temp = q->head;
+		q->head = q->head->next;
+		return temp;
+	}
+}
---
+++ Queue.h
@@ -0,0 +1,23 @@
+#pragma once
+#include<iostream>
+using namespace std;
+
+
+class Node {
+public:
+	string instruction;
+	Node* next;
+
+	Node(string instruction) {};
+};
+
+class Queue {
+public:
+	Node* head;
+	Node* tail;
+
+	Queue() {};
+};
+
+void push(Queue* q, Node* node);
+Node* pop(Queue* q);
