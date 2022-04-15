253bc2db1df179e7577559dde485302192ff4c96
Are-u-writing-bugs <chaseshen0626@gmail.com>
2022-04-15 01:55:10 -0700

    read file

--- main.cpp
+++ main.cpp
@@ -2,11 +2,14 @@
 // Created by 乔嘉龙 on 2022/4/14.
 //
 
+#ifndef QUEUE
 #include "Queue.h"
-#include <iostream>
-#include <string>
+#define QUEUE
+#endif // !QUEUE
+
 #include <fstream>
-using namespace std;
+#include <vector>
+//using namespace std;
 
 //create 5 stages queue
 //Queue* IFQueue = new Queue();
@@ -16,24 +19,65 @@ using namespace std;
 //Queue* WBQueue = new Queue();
 
 
-void simulation(string file_name, int wide, int start_line, int total_simulate_lines){
-    //
-    ifstream read_file(file_name);
-    string line;
-    //go to specific line
-    int current_line = 0;
-    while(current_line < start_line){
-        getline(read_file,line);
-        cout<< line <<endl;
-        current_line++;
-    }
-    //start pipeline
+void simulation(string file_name, int width, int start_line, int total_simulate_lines) {
+	
+	//ifstream read_file(file_name);
+	//string line;
+	////go to specific line
+	//int current_line = 0;
+	//while(current_line < start_line){
+	//	getline(read_file,line);
+	//	cout<< line <<endl;
+	//	current_line++;
+	//}
+
+	ifstream infile;
+	infile.open(file_name, ifstream::in);
+	if (infile.is_open()) {
+		vector<string> vStr;
+		string content;
+
+		int current_line_infile = 0;
+		int line_count = 0;
+		while (line_count<total_simulate_lines)
+		{
+			getline(infile, content);
+			
+			if (current_line_infile >= start_line && line_count<total_simulate_lines) {
+				vStr.push_back(content);
+				line_count++;
+			}
+
+			current_line_infile++;
+
+			
+			cout << "current line in file: " << current_line_infile << endl;
+		}
+
+		cout << "line count: " << line_count << endl;
+		for (auto line : vStr)
+		{
+			cout << line << endl;
+		}
+	}
+	infile.close();
 
 
 }
 
-int main(){
-    cout << "start" << endl;
-    simulation("srv_0",1,0,3);
-    return 0;
+int main(int argc, char* argv[]) {
+	string filename = "srv_0";
+	//string filename = "test.txt";
+
+	int width = 1;
+	int from_line = 50;
+	int total_line = 100;
+
+
+	cout << "start simulation" << endl;
+	simulation(filename, width, from_line, total_line);
+
+
+	system("pause");
+	return 0;
 }
