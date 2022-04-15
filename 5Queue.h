575f89b5e5406549de10a92962877f8ea16d7bfd
Jialong Qiao <jialongq@sfu.ca>
2022-04-14 19:33:21 -0700

    simulation function

---
+++ 5Queue.h
@@ -0,0 +1,27 @@
+//
+// Created by 乔嘉龙 on 2022/4/14.
+//
+
+#ifndef CMPT305_PROJECT2_5QUEUE_H
+#define CMPT305_PROJECT2_5QUEUE_H
+
+#endif //CMPT305_PROJECT2_5QUEUE_H
+#include "Queue.h"
+#include <iostream>
+
+using namespace std;
+
+//create 5 stages queue
+void create_5_Queue(){
+    Queue* IFQueue = new Queue();
+    Queue* IDQueue = new Queue();
+    Queue* EXQueue = new Queue();
+    Queue* MEMQueue = new Queue();
+    Queue* WBQueue = new Queue();
+    //这五个队列要全文件可用，忘了怎么设置了
+}
+
+void initializeQueue(){
+    //读取指令？
+}
+
---
+++ main.cpp
@@ -0,0 +1,25 @@
+//
+// Created by 乔嘉龙 on 2022/4/14.
+//
+
+#include "Queue.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+//create 5 stages queue
+Queue* IFQueue = new Queue();
+Queue* IDQueue = new Queue();
+Queue* EXQueue = new Queue();
+Queue* MEMQueue = new Queue();
+Queue* WBQueue = new Queue();
+
+
+void simulation(stringn file_name, int wide, int start_line,int total_simulate_lines){
+    //
+    ifstream read_file(file_name);
+    //
+
+}
+
+
